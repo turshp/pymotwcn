07761e0e259bc62e99712352f35dcd152e545067
shengyan1985 <shengyan1985@e4b09960-ae51-11dd-aaf8-13f29377832e>
2008-12-23 10:24:37 +0000

    删除output

--- trunk/sphinx-project2/src/conf.py
+++ trunk/sphinx-project2/src/conf.py
@@ -41,8 +41,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PyMOTW'
-copyright = u'2008, caijunjie'
+project = u'PyMOTW中文版'
+copyright = u'2008, vbarter ＆ liz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -168,8 +168,8 @@ htmlhelp_basename = 'PyMOTWdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'PyMOTW.tex', ur'PyMOTW Documentation',
-   ur'caijunjie', 'manual'),
+  ('index', 'PyMOTW.tex', ur'PyMOTW 文档',
+   ur'vbarter & liz', '手册'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
